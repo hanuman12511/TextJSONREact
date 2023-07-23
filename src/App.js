2246341b544915509f33e9271d4b664a93d1e4aa
hanuman12511 <er.hanumansaini@gmail.com>
2023-07-23 18:27:57 +0530

    werwer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
+import categories from './data/categories.json';
+import subcategories from './data/subcategories.json';
 function App() {
+ 
+  let data = []
+  let data1=[]
+ categories.forEach((categories)=>{
+    
+    subcategories.forEach((subcategories)=>{
+        if(categories._id.$oid === subcategories.categoryId){
+
+       data1.push(subcategories.categoryId)
+      }
+    })
+  console.log('====================================');
+  console.log("sub=",data1);
+  console.log('====================================');
+  
+    data.push( data1)
+  })
+ 
+  console.log('====================================');
+  console.log("data=",data);
+  console.log('====================================');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>app</h1>
     </div>
   );
 }
